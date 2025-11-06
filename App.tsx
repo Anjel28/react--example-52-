bbbe7ad47f7dfec9430a95d3d844d6ab0caf1940
Anjel28 <anjelchaudhary28@gmail.com>
2025-11-06 18:49:00 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+//import MenuList from './components/MenuList';
+//import UserList from './components/UserList';
+//import  ToolTripWithUseEffect  from './components/ToolTipWithUseEffect';
+//import ChatBox from './components/ChatBox';
+//import ToolTipWithUseLayoutEffect from './components/ToolTipWithUseLayoutEffect';
+//import Cart from './components/Cart';
+import MultiStepForm from './components/MultiStepForm';
+
+function App() {
+  return (
+    <div>
+     {/* <ToolTipWithUseLayoutEffect/>*/}
+        {/*<ChatBox messages={["Ankle"]} />*/}
+     <MultiStepForm/>
+      {/*<Cart />*/}
+      {/*<ToolTripWithUseEffect/>*/}
+     {/* <UserList/> */}
+      {/*<MenuList />*/}
+    </div>
+     
+  );
+}
+
+export default App;
